319dbc52ca8bf363d71894f9b9a5f404ed640142
Oliver Albertini <oalbertini@pivotal.io>
2018-08-04 22:04:45 -0700

    Backfill specs

--- lita-interrupt/spec/lita/handlers/interrupt_spec.rb
+++ lita-interrupt/spec/lita/handlers/interrupt_spec.rb
@@ -2,43 +2,78 @@ require 'spec_helper'
 
 describe Lita::Handlers::Interrupt, lita_handler: true do
   describe '#run' do
-    it { is_expected.to route_command('hey').to(:handle_interrupt) }
+    let(:maester) { Lita::User.create('U9298ANLQ', name: 'maester_luwin') }
+    let(:sam) { Lita::User.create('U93MFAV9V', name: 'sam') }
+    let(:jon) { Lita::User.create('U1BSCLVQ1', name: 'jon') }
+    let(:tyrion) { Lita::User.create('U5062MBLE', name: 'tyrion') }
+    let(:jaime) { Lita::User.create('U8FE4C6Z7', name: 'jaime') }
+    let(:list) { Trello::List.new(list_details) }
+    before do
+      allow(Trello::Member).to receive(:find).with('jonsnow').and_return(Trello::Member.new(jon_details))
+      allow(Trello::Member).to receive(:find).with('samwelltarley').and_return(Trello::Member.new(sam_details))
+      allow(Trello::Member).to receive(:find).with('jaimelannister').and_return(Trello::Member.new(jaime_details))
+      allow(Trello::Member).to receive(:find).with('tyrionlannister').and_return(Trello::Member.new(tyrion_details))
+      allow_any_instance_of(Trello::Member).to receive(:boards).and_return([Trello::Board.new(name: 'Game of Boards')])
+      allow_any_instance_of(Trello::Board).to receive(:lists).and_return([list])
+      allow(Trello::List).to receive(:find).with(list.id).and_return(list)
+      allow(list)
+        .to receive(:cards)
+        .and_return([
+      Trello::Card.new(card_details_array[0]),
+      Trello::Card.new(card_details_array[1]),
+      Trello::Card.new(card_details_array[2])
+      ])
+    end
 
-    describe 'when nobody is interrupt' do
+    it 'routes the command' do
+      is_expected.to route_command('hey').to(:handle_interrupt)
+    end
+
+    describe 'when tyrion & jaime are the interrupt pair' do
       before do
-        allow(Trello::List).to receive(:find).with('samwelltarley').and_return(trello_sam)
+        robot.trigger(:connected)
+      end
+      it 'pings the interrupt pair only' do
+        send_command('hello hello hello', as: maester)
+        expect(replies.last).to eq("<@#{tyrion.id}> <@#{jaime.id}>: you have an interrupt from <@#{maester.id}> ^^")
+      end
+    end
+
+    describe 'when there is no interrupt list' do
+      before do
+        allow(list)
+          .to receive(:cards)
+          .and_return([
+        Trello::Card.new(card_details_array[1]),
+        Trello::Card.new(card_details_array[2])
+        ])
+      end
+      it 'raises an error' do
+        expect { robot.trigger(:connected) }.to raise_error(%q(
+              Interrupt list not found!
+              Your team trello board needs a list with a card titled 'Interrupt'!
+                                                            ))
+      end
+    end
+
+    describe 'when there is no interrupt list' do
+      before do
+        allow(list)
+          .to receive(:cards)
+          .and_return([
+        Trello::Card.new(card_details_array[0])
+        ])
+        robot.trigger(:connected)
       end
       it 'pings the whole team' do
-        send_command('hello hello hello')
-        expect(replies.last).to eq('')
+        send_command('hello hello hello', as: maester)
+        expect(replies.last).to eq("<@#{jon.id}> <@#{sam.id}> <@#{tyrion.id}> <@#{jaime.id}>: you have an interrupt from <@#{maester.id}> ^^")
       end
     end
 
-    def user_details
-      {
-        'id'         => 'abcdef123456789012345678',
-        'fullName'   => 'Samwell Tarley',
-        'username'   => 'samwelltarley',
-        'intials'    => 'ST',
-        'avatarHash' => 'abcdef1234567890abcdef1234567890',
-        'bio'        => 'a rather famous user',
-        'url'        => 'https://trello.com/samwelltarley',
-        'email'      => 'samwelltarley@thewall.com'
-      }
-    end
     describe 'someone requests to be added to team_members' do
-      let!(:trello_sam) do
-        Trello::Member.new(user_details)
-      end
-      let!(:sam) { Lita::User.create('U9Z2QJL1F', name: 'sam') }
-
-      before do
-        allow(Trello::Member).to receive(:find).with('samwelltarley').and_return(trello_sam)
-      end
-
       it 'adds them' do
         send_command('add samwelltarley to BAM', as: sam)
-
         expect(replies.last).to eq(%Q(I have linked trello user 'samwelltarley' with <@#{sam.id}>!))
       end
     end
--- lita-interrupt/spec/spec_helper.rb
+++ lita-interrupt/spec/spec_helper.rb
@@ -1,3 +1,8 @@
+ENV['TEAM_MEMBERS_HASH'] = "jonsnow:U1BSCLVQ1,samwelltarley:U93MFAV9V,tyrionlannister:U5062MBLE,jaimelannister:U8FE4C6Z7"
+ENV['TRELLO_DEVELOPER_PUBLIC_KEY'] = 'some_public_key'
+ENV['TRELLO_MEMBER_TOKEN'] = 'some_member_token'
+ENV['TRELLO_BOARD_NAME'] = 'Game of Boards'
+
 require "lita-interrupt"
 require "lita/rspec"
 
@@ -9,4 +14,110 @@ RSpec.configure do |config|
   config.before do
     registry.register_hook(:validate_route, Lita::Extensions::ExclusiveRoute)
   end
+end
+
+def jon_details
+  {
+    'id'         => 'abcde1f23457689021346578',
+    'fullName'   => 'Jon Snow',
+    'username'   => 'jonsnow',
+    'intials'    => 'JS',
+    'avatarHash' => 'abcdef1234567890abcdef1234567890',
+    'bio'        => 'a rather famous user',
+    'url'        => 'https://trello.com/jonsnow',
+    'email'      => 'jonsnow@thewall.com'
+  }
+end
+def sam_details
+  {
+    'id'         => 'abcdef123456789012345678',
+    'fullName'   => 'Samwell Tarley',
+    'username'   => 'samwelltarley',
+    'intials'    => 'ST',
+    'avatarHash' => 'abcdef1234567890abcdef1234567890',
+    'bio'        => 'a rather famous user',
+    'url'        => 'https://trello.com/samwelltarley',
+    'email'      => 'samwelltarley@thewall.com'
+  }
+end
+def jaime_details
+  {
+    'id'         => 'abdcfe132465879012346587',
+    'fullName'   => 'Jaime Lannister',
+    'username'   => 'jaimelannister',
+    'intials'    => 'JL',
+    'avatarHash' => 'abcdef1234567890abcdef1234567890',
+    'bio'        => 'a rather famous user',
+    'url'        => 'https://trello.com/jaimelannister',
+    'email'      => 'jaimelannister@golden.com'
+  }
+end
+def tyrion_details
+  {
+    'id'         => 'abcedf132465798021354687',
+    'fullName'   => 'Tyrion Lannister',
+    'username'   => 'tyrionlannister',
+    'intials'    => 'TL',
+    'avatarHash' => 'abcdef1234567890abcdef1234567890',
+    'bio'        => 'a rather famous user',
+    'url'        => 'https://trello.com/tyrionlannister',
+    'email'      => 'tyrionlannister@golden.com'
+  }
+end
+def list_details
+  {
+    'id'           => 'abcdef123456789123456789',
+    source_list_id: 'abcdef123456789123456780'
+  }
+end
+def board_details
+  {
+    'id'               => 'abcdef123456789123456789',
+    organization_id: 'abcdef123456789123456789'
+  }
+end
+def card_details_array
+  [
+    {
+      name:                   'Interrupt',
+      list_id:                'abcdef123456789123456789',
+      desc:                   'This is the interrupt card',
+      member_ids:             [],
+      card_labels:            [ 'abcdef123456789123456789',
+                                'bbcdef123456789123456789',
+                                'cbcdef123456789123456789',
+                                'dbcdef123456789123456789' ],
+                                due:                    Date.today,
+                                pos:                    12,
+                                source_card_id:         'abcdef1234567891234567890',
+                                source_card_properties: 'checklist,members'
+    },
+    {
+      name:                   'Tyrion Lannister',
+      list_id:                'abcdef123456789123456789',
+      desc:                   'Tyrion Lannister personal card.',
+      member_ids:             ['tyrionlannister'],
+      card_labels:            [ 'abcdef123456789123456789',
+                                'bbcdef123456789123456789',
+                                'cbcdef123456789123456789',
+                                'dbcdef123456789123456789' ],
+                                due:                    Date.today,
+                                pos:                    12,
+                                source_card_id:         'abcdef1234567891234567890',
+                                source_card_properties: 'checklist,members'
+    },
+    {
+      name:                   'Jaime Lannister',
+      list_id:                'abcdef123456789123456789',
+      desc:                   'Jaime Lannister personal card.',
+      member_ids:             ['jaimelannister'],
+      card_labels:            [ 'abcdef123456789123456789',
+                                'bbcdef123456789123456789',
+                                'cbcdef123456789123456789',
+                                'dbcdef123456789123456789' ],
+                                due:                    Date.today,
+                                pos:                    12,
+                                source_card_id:         'abcdef1234567891234567890',
+                                source_card_properties: 'checklist,members'
+    }]
 end
